b3a883ac175abe15b79f4dce114e93a45657f1e7
Diego <diego.dellavecchia3@gmail.com>
2025-06-16 22:04:18 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"spellcheck/trees"
 	"time"
 )
+
+var ascii = `
+ _                             _   _          _         ____  _     _                       
+| |    _____   _____ _ __  ___| |_| |__   ___(_)_ __   |  _ \(_)___| |_ __ _ _ __   ___ ___ 
+| |   / _ \ \ / / _ \ '_ \/ __| __| '_ \ / _ \ | '_ \  | | | | / __| __/ _' | '_ \ / __/ _ \
+| |__|  __/\ V /  __/ | | \__ \ |_| | | |  __/ | | | | | |_| | \__ \ || (_| | | | | (_|  __/
+|_____\___| \_/ \___|_| |_|___/\__|_| |_|\___|_|_| |_| |____/|_|___/\__\__,_|_| |_|\___\___|
+`
 
 func main() {
 
@@ -25,18 +35,31 @@ func main() {
 	})
 
 	tree.InsertList(words[1:])
-	fmt.Println("Wörter in Baum eingefügt:", len(words))
 
-	fmt.Println("Geben Sie ein Wort ein:")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	fmt.Println("Eingegebenes Wort:", input)
-	results := tree.Lookup(input, 2)
-	fmt.Println("Ähnliche Wörter:")
+	for i := 0; i >= 0; i++ {
 
-	for _, word := range results {
-		fmt.Println(word)
-		time.Sleep(200 * time.Millisecond) // Pause for 1 second
+		fmt.Println(ascii)
+
+		fmt.Println("Wörter in Baum eingefügt:", len(words))
+		if i == 0 {
+			fmt.Println("Geben Sie ein Wort ein:")
+		} else {
+			fmt.Println("Geben Sie ein anderes Wort ein:")
+		}
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		fmt.Println("Eingegebenes Wort:", input)
+		results := tree.Lookup(input, 2)
+		fmt.Println("Ähnliche Wörter:")
+
+		for _, word := range results {
+			fmt.Println(word)
+			time.Sleep(200 * time.Millisecond)
+		}
+		time.Sleep(5 * time.Second)
+
+		clearTerminal()
+
 	}
 
 }
@@ -53,4 +76,17 @@ func readJSONFile(filePath string) ([]string, error) {
 	}
 
 	return result, nil
+}
+func clearTerminal() {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default: // Unix-like (Linux, macOS)
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
